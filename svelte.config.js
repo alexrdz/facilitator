965363ea4cacabf5250b2cb15f0619c3916cb69b
AlexRdz <drinkhorchata@gmail.com>
2025-08-31 23:48:20 -0400

    fix netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-netlify";
 
-const netlify = require("@sveltejs/adapter-netlify");
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
@@ -11,7 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: netlify(),
+    adapter: adapter(),
   },
 };
 
